66be47be8d6bf9861fdfafbc3634ebd8d036de33
PrakharTeotia <prakharteotia77@gmail.com>
2025-05-02 22:54:31 +0530

    changed host

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -10,7 +10,7 @@ function handleLogin(event) {
         password: password
     };
 
-    fetch('http://localhost:8080/api/v1/users/login', {
+    fetch('https://videoconferencingapp-1.onrender.com/api/v1/users/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
